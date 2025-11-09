96c3189e34ed92fa1abd4a33a7f46b8a8928bb37
aldrin <tolentinoaldrin369@gmail.com>
2025-11-10 00:17:06 +0800

    done solve

--- index.php
+++ index.php
@@ -4,28 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HCDC</title>
+   
 </head>
-<body style="background-color: navy;" >
-<<<<<<< HEAD
-    <h1 style="text-align: center; color:azure; font-size:200px " >Welcome to HCDC!</h1>
-=======
-    <h1 style="text-align: center; color:azure; font-size:200px; text-decoration:underline; " >Welcome to HCDC nga pianka gwapo</h1>
->>>>>>> 9c7dc08792ea91bbca372530ae935e3b88a6280d
+<body>
+    
     <p style="text-align: center; color:azure; font-size:50px " >Health Care Data Center</p>
     <div style="text-align: center; margin-top: 50px;">
         <a href="login.php" style="font-size:30px; color: yellow; text-decoration: none; margin-right: 20px;">Login</a>
         <a href="register.php" style="font-size:30px; color: yellow; text-decoration: none;">Register</a>
-        <h1>dyfhbaocndaad</h1>
-        <br>
-        <div>
-            <table>
-                <tr>
-                    <tr>dbjh s dc </tr>
-                </tr>
-            </table>
-                  
-        </div>
-        
+  
+   
      
 </body>
 </html>
