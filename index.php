6a5fbb9897b5deea2e797a3aa1de76242c32305a
aldrin <tolentinoaldrin369@gmail.com>
2025-11-09 23:10:47 +0800

    i added the heading and the login reg

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HCDC</title>
 </head>
-<body>
-    <h1>Welcome to HCDC</h1>
+<body style="background-color: navy;" >
+    <h1 style="text-align: center; color:azure; font-size:200px " >Welcome to HCDC</h1>
+    <p style="text-align: center; color:azure; font-size:50px " >Health Care Data Center</p>
+    <div style="text-align: center; margin-top: 50px;">
+        <a href="login.php" style="font-size:30px; color: yellow; text-decoration: none; margin-right: 20px;">Login</a>
+        <a href="register.php" style="font-size:30px; color: yellow; text-decoration: none;">Register</a>
+     
 </body>
 </html>
